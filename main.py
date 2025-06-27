b213e29eeaf7fd46df253080c7f222a4ec579be2
romansindeev <sindeevr5@gmail.com>
2025-06-27 12:19:02 +0300

    Промежуточный чек

--- main.py
+++ main.py
@@ -1,8 +1,36 @@
 import aiohttp
 import asyncio
+import logging
+from logging.handlers import TimedRotatingFileHandler
+import os
 
 import database as db
 from config import BASE_URL, GUID
+
+log_dir = './logs/'
+os.makedirs(log_dir, exist_ok=True)
+
+log_file = os.path.join(log_dir, 'policy.log')
+
+logger = logging.getLogger('PolicyAutocheck')
+logger.setLevel(logging.INFO)
+
+handler = TimedRotatingFileHandler(
+    log_file,
+    when='midnight',
+    interval=1,    
+    backupCount=14,
+    encoding='utf-8',
+    utc=False   
+)
+
+formatter = logging.Formatter(
+    fmt='%(asctime)s | %(levelname)s | %(name)s | %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
+handler.setFormatter(formatter)
+
+logger.addHandler(handler)
 
 async def sendIdentityPatient(lpuId, client: dict) -> tuple[str, str]:
     client["lpuId"] = lpuId
@@ -43,8 +71,15 @@ async def main():
     clients = await db.getClients()
     lpuId = await db.getLpuId()
 
-    result = await sendIdentityPatient(lpuId, clients[0][1])
-    await db.writeCheckPolicy(result[0], clients[0][0], result[1])
+    for client in clients:
+        try:
+            result = await sendIdentityPatient(lpuId, client[1])
+            await db.writeCheckPolicy(result[0], client[0], result[1])
+            logging.info(f"Клиент {client[0]} успешно отправлен")
+
+        except Exception as e:
+            logging.error(f"Клиента {client[0]} не удалось отправить: {str(e)}")
+
 
 if __name__=="__main__":
     asyncio.run(main())
